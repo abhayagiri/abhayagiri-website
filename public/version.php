ca02adc8ec879e881be771288d041ee34d3c065c
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2016-07-31 12:41:14 -0700

    Fix caching for feeds

--- public/version.php
+++ public/version.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-header('Content-Type: text/plain;', true);
+header('Content-Type: text/plain; charset=utf-8', true);
 
 echo Abhayagiri\Util::getVersion();
 
