a92466751548f702f36c80d125aaec740d4a343c
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2016-07-31 10:57:06 -0700

    Do not cache

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
+header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+
 session_start();
 
 ?>
