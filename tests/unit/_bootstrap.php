62709cf4093efa6e4237a4dd959373ca197aa477
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2016-09-08 14:29:11 -0700

    Use regular .env file for testing

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__.'/../../bootstrap/autoload.php';
 $app = require __DIR__.'/../../bootstrap/app.php';
-$app->loadEnvironmentFrom('.env.testing');
+$app->loadEnvironmentFrom('.env');
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
