87f340767ea95498ea9cf769b2ff59f2aaa3363b
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2016-09-11 15:45:53 -0700

    Remove conditions on Mahapanel oauth test

--- tests/functional/MahapanelCept.php
+++ tests/functional/MahapanelCept.php
@@ -3,12 +3,7 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('to ensure mahapanel is secure');
 $I->amOnPage('/mahapanel');
-
-if (env('AUTH_GOOGLE_CLIENT_ID')) {
-    $I->see('Google');
-} else {
-    $I->see('Missing required parameter: client_id');
-}
+$I->see('Google');
 $I->seeCurrentUrlMatches('~^/o/oauth2/auth~');
 
 $I->amOnPage('/mahapanel_bypass?email=root@localhost');
