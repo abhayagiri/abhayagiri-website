e5018f794a5b6bf63df1f9dc14d68cd68cf563c1
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2017-07-04 16:51:53 -0700

    Fix unit tests when optimized php exists

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__.'/../../bootstrap/autoload.php';
 $app = require __DIR__.'/../../bootstrap/app.php';
 $app->loadEnvironmentFrom('.env');
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
