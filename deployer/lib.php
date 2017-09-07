3882413b2fa6e51b186dda550a72fac291addbc0
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2017-09-07 12:54:17 -0700

    Remove references to boostrap/autoload.php

--- deployer/lib.php
+++ deployer/lib.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../bootstrap/app.php';
 $app->loadEnvironmentFrom('.env');
