ada75b986547c16dc40b90f863141e4a3508f61b
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2017-09-13 07:42:33 -0700

    Fix header CSRF problems

--- resources/views/page/header.php
+++ resources/views/page/header.php
@@ -22,10 +22,10 @@ if (Lang::locale() === 'th') {
         <!--meta-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo $_title ?></title>
-        <meta name="description" content="<?php echo $_meta_description ?>">
+        <title><?= e($_title) ?></title>
+        <meta name="description" content="<?= e($_meta_description) ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <meta name="csrf-token" content="<?php echo csrf_token() ?>">
+        <meta name="csrf-token" content="<?= e(csrf_token()) ?>">
         <!--/meta-->
 
         <!--[if lt IE 9]>
@@ -36,23 +36,23 @@ if (Lang::locale() === 'th') {
         <link rel="stylesheet" href="/css/font-awesome.css">
         <link rel="stylesheet" href="/css/bootstrap.css">
         <link rel="stylesheet" href="/css/bootstrap-responsive.css">
-        <link rel="stylesheet" href="/css/main.css?<?php echo $versionStamp ?>">
-        <link rel="stylesheet" href="/css/mods.css?<?php echo $versionStamp ?>">
+        <link rel="stylesheet" href="/css/main.css?<?= e($versionStamp) ?>">
+        <link rel="stylesheet" href="/css/mods.css?<?= e($versionStamp) ?>">
         <?php if (Lang::locale() === 'th') { ?>
-            <link rel="stylesheet" href="/th/css/thai.css?<?php echo $versionStamp ?>">
+            <link rel="stylesheet" href="/th/css/thai.css?<?= e($versionStamp) ?>">
         <?php } ?>
         <script>document.cookie = 'resolution=' + Math.max(screen.width, screen.height) + '; path=/';</script>
         <!--/css-->
 
         <script>
             //Global Variables
-            _page = "<?php echo $_page ?>";
-            _subpage = "<?php echo $_subpage ?>";
-            _subsubpage = "<?php echo $_subsubpage ?>";
-            _type = "<?php echo $_type ?>";
-            _icon = "<?php echo $_icon ?>";
-            _action = "<?php echo $_action ?>";
-            history.replaceState({action: _action, page: _page, entry: _subpage, album: _subpage, event: _subpage, resident: _subsubpage, subpage: _subpage}, null, "<?php echo $base ?>" + _page + "/" + _subpage + ((_subsubpage == "") ? "" : ("/" + _subsubpage)));
+            _page = <?= json_encode($_page) ?>;
+            _subpage = <?= json_encode($_subpage) ?>;
+            _subsubpage = <?= json_encode($_subsubpage) ?>;
+            _type = <?= json_encode($_type) ?>;
+            _icon = <?= json_encode($_icon) ?>;
+            _action = <?= json_encode($_action) ?>;
+            history.replaceState({action: _action, page: _page, entry: _subpage, album: _subpage, event: _subpage, resident: _subsubpage, subpage: _subpage}, null, <?= json_encode($base) ?> + _page + "/" + _subpage + ((_subsubpage == "") ? "" : ("/" + _subsubpage)));
             //Remove '#' from URI
             currlocation = window.location.href;
             if (currlocation.indexOf("#") !== -1) {
