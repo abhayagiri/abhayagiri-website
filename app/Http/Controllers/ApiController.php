2a95d9f69e7aa6660bce7b8c8c6c051d0ac0db07
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2017-09-17 13:46:44 -0700

    Include subjects in /api/subject-groups

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -92,6 +92,8 @@ class ApiController extends Controller
         return $this->camelizeResponse(
             SubjectGroup::withoutGlobalScope(TitleEnScope::class)
             ->orderBy('rank')->orderBy('title_en')
+            // TEMP add to make it easy to grab the first/default subject
+            ->with('subjects')
             ->get());
     }
 
