2def78746d5355b0f5443d0ef4b85e81a78d81ae
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2017-11-07 16:46:33 -0800

    Add basic test stub for albums

--- tests/api/AlbumsCept.php
+++ tests/api/AlbumsCept.php
@@ -2,6 +2,7 @@
 $I = new ApiTester($scenario);
 $I->wantTo('get albums via API');
 
+// TODO
 $I->sendGET('/albums');
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 $I->seeResponseIsJson();
@@ -10,3 +11,10 @@ $I->assertGreaterThan(-1, count($albums));
 
 $I->sendGET('/albums/abc');
 $I->seeResponseCodeIs(\Codeception\Util\HttpCode::NOT_FOUND);
+
+// TODO
+$I->sendGET('/albums?searchText=buddha');
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+$I->seeResponseIsJson();
+$albums = $I->grabDataFromResponseByJsonPath('*');
+$I->assertGreaterThan(-1, count($albums));
