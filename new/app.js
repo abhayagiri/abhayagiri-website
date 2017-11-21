3da0ae370cab8294f2e8dcc03da932bfa2de0b80
Dushan Perera <dushan.v.perera@gmail.com>
2017-11-21 13:33:28 -0800

    fixed issue with links being unclickable on talk list

--- new/app.js
+++ new/app.js
@@ -61,7 +61,7 @@ class App extends Component {
 
 
                 {/* Gallery */}
-                <Route name="Gallery" path="gallery">
+                <Route path="gallery">
                     <IndexRoute name="Albums" component={AlbumList}/>
                     <Route name="" path=":albumId" component={Album} />
                 </Route>
