52a37a1a43b3baba792245d7465dec64f3b13c25
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2018-01-07 12:35:46 -0800

    Use class-style HOC for withGlobals (and fix indentation)

--- new/components/shared/globals/globals.js
+++ new/components/shared/globals/globals.js
@@ -34,7 +34,7 @@
  *         <GlobalsProvider state={{ 'color': this.state.color }}>
  *             ...
  *         </GlobalsProvider>
- * 
+ *
  * You can use the GlobalsListener to get a global as a prop:
  *
  *     render() {
@@ -51,7 +51,7 @@
  * - constructor: initial get()
  * - componentDidMount(): subscriptions and initial set() from props
  * - componentWillReceiveProps(): subsequent set() from props
- * - componentWillUnmount(): remove subscriptions  
+ * - componentWillUnmount(): remove subscriptions
  *
  * References
  * ----------
@@ -63,167 +63,180 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import hoistStatics from 'hoist-non-react-statics'
 import Emitter from 'tiny-emitter';
 
 class GlobalsStore {
 
-	constructor() {
-		this.state = {};
-		this.emitter = new Emitter();
-	}
+    constructor() {
+        this.state = {};
+        this.emitter = new Emitter();
+    }
 
-	get(key) {
-		return this.state[key];
-	}
+    get(key) {
+        return this.state[key];
+    }
 
-	set(key, value) {
-		this.state[key] = value;
-		this.emitter.emit(key, value);
-		return value;
-	}
+    set(key, value) {
+        this.state[key] = value;
+        this.emitter.emit(key, value);
+        return value;
+    }
 
-	subscribe(key, callback) {
-		return this.emitter.on(key, callback);
-	}
+    subscribe(key, callback) {
+        return this.emitter.on(key, callback);
+    }
 
-	unsubscribe(key, callback) {
-		return this.emitter.off(key, callback);
-	}
+    unsubscribe(key, callback) {
+        return this.emitter.off(key, callback);
+    }
 
 }
 
 export class GlobalsApp extends Component {
 
-	static childContextTypes = {
-		globals: PropTypes.object.isRequired
-	}
+    static childContextTypes = {
+        globals: PropTypes.object.isRequired
+    }
 
-	static propTypes = {
-		children: PropTypes.node.isRequired
-	}
+    static propTypes = {
+        children: PropTypes.node.isRequired
+    }
 
-	constructor(props, context) {
-		super(props, context);
-		this.globals = new GlobalsStore();
-	}
+    constructor(props, context) {
+        super(props, context);
+        this.globals = new GlobalsStore();
+    }
 
-	getChildContext() {
-		return { globals: this.globals };
-	}
+    getChildContext() {
+        return { globals: this.globals };
+    }
 
-	render() {
-		return React.Children.only(this.props.children);
-	}
+    render() {
+        return React.Children.only(this.props.children);
+    }
 
 }
 
 export class GlobalsProvider extends Component {
 
-	static contextTypes = {
-		globals: PropTypes.object.isRequired
-	}
+    static contextTypes = {
+        globals: PropTypes.object.isRequired
+    }
 
-	static propTypes = {
-		children: PropTypes.node.isRequired,
-		state: PropTypes.object.isRequired
-	}
+    static propTypes = {
+        children: PropTypes.node.isRequired,
+        state: PropTypes.object.isRequired
+    }
 
-	componentDidMount() {
-		for (const key in this.props.state) {
-			this.context.globals.set(key, this.props.state[key]);
-		}
-	}
+    componentDidMount() {
+        for (const key in this.props.state) {
+            this.context.globals.set(key, this.props.state[key]);
+        }
+    }
 
-	componentWillReceiveProps(nextProps) {
-		for (const key in nextProps.state) {
-			this.context.globals.set(key, nextProps.state[key]);
-		}
-	}
+    componentWillReceiveProps(nextProps) {
+        for (const key in nextProps.state) {
+            this.context.globals.set(key, nextProps.state[key]);
+        }
+    }
 
-	render() {
-		return React.Children.only(this.props.children);
-	}
+    render() {
+        return React.Children.only(this.props.children);
+    }
 
 }
 
 export class GlobalsListener extends Component {
 
-	static contextTypes = {
-		globals: PropTypes.object.isRequired
-	}
+    static contextTypes = {
+        globals: PropTypes.object.isRequired
+    }
 
-	static propTypes = {
-		children: PropTypes.func.isRequired,
-		map: PropTypes.oneOfType([
-			PropTypes.object,
-			PropTypes.array,
-			PropTypes.string
-		])
-	}
+    static propTypes = {
+        children: PropTypes.func.isRequired,
+        map: PropTypes.oneOfType([
+            PropTypes.object,
+            PropTypes.array,
+            PropTypes.string
+        ])
+    }
 
     constructor(props, context) {
         super(props, context);
         this.setMap(props.map);
 
         let state = {};
-		for (const key in this.map) {
-        	state[key] = context.globals.get(key);
+        for (const key in this.map) {
+            state[key] = context.globals.get(key);
         }
         this.state = state;
         this.listeners = {};
     }
 
     setMap(map) {
-		if (Array.isArray(map)) {
-			this.map = map.reduce((map, key) => {
-				map[key] = key;
-				return map;
-			}, {});
-		} else if (typeof map === 'object') {
-			this.map = map;
-		} else if (map) {
-			this.map = { [map]: map };
-		} else {
-			this.map = {};
-		}
+        if (Array.isArray(map)) {
+            this.map = map.reduce((map, key) => {
+                map[key] = key;
+                return map;
+            }, {});
+        } else if (typeof map === 'object') {
+            this.map = map;
+        } else if (map) {
+            this.map = { [map]: map };
+        } else {
+            this.map = {};
+        }
     }
 
-	componentDidMount() {
-		const { map } = this;
-		for (const key in map) {
-			const listener = (value) => {
-				this.setState({ [map[key]]: value });
-			};
-			this.listeners[key] = listener;
-			this.context.globals.subscribe(key, listener);
-	    }
-	}
+    componentDidMount() {
+        const { map } = this;
+        for (const key in map) {
+            const listener = (value) => {
+                this.setState({ [map[key]]: value });
+            };
+            this.listeners[key] = listener;
+            this.context.globals.subscribe(key, listener);
+        }
+    }
 
-	componentWillUnmount() {
-		for (const key in this.map) {
-	        this.context.globals.unsubscribe(key, this.listeners[key]);
-	    }
-	}
+    componentWillUnmount() {
+        for (const key in this.map) {
+            this.context.globals.unsubscribe(key, this.listeners[key]);
+        }
+    }
 
-	render() {
-		let props = {};
-		for (const key in this.map) {
-			props[this.map[key]] = this.state[key];
-		}
-		return this.props.children(props);
-	}
+    render() {
+        let props = {};
+        for (const key in this.map) {
+            props[this.map[key]] = this.state[key];
+        }
+        return this.props.children(props);
+    }
 
 }
 
 export function withGlobals(SubComponent, map) {
-	return (parentProps) => {
-		return (
-			<GlobalsListener map={map}>
-				{(props) => {
-					props = Object.assign(props, parentProps);
-					return <SubComponent {...props} />;
-				}}
-			</GlobalsListener>
-		);
-	};
+
+    const displayName = (SubComponent.displayName || SubComponent.name
+        || 'Component');
+
+    class WithGlobals extends Component {
+
+        static displayName = `WithGlobals(${displayName})`;
+
+        render() {
+            const parentProps = this.props;
+            return (
+                <GlobalsListener map={map}>
+                    {(props) => {
+                        props = Object.assign(props, parentProps);
+                        return <SubComponent {...props} />;
+                    }}
+                </GlobalsListener>
+            );
+        }
+    };
+
+    return hoistStatics(WithGlobals, SubComponent);
 }
--- new/components/shared/globals/globals.test.js
+++ new/components/shared/globals/globals.test.js
@@ -3,54 +3,54 @@ import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 
 import { GlobalsApp, GlobalsProvider, GlobalsListener,
-	     withGlobals } from './globals';
+         withGlobals } from './globals';
 
 describe('withGlobals', () => {
 
-	class A extends Component {
-		static contextTypes = {
-			globals: PropTypes.object.isRequired
-		}
-		componentDidMount() {
-			this.context.globals.set('color', 'red');
-		}
-		makeBlue() {
-			this.context.globals.set('color', 'blue');
-		}
-		render() {
-			return (
-				<div>
-					<div>{this.props.children}</div>
-					<button onClick={this.makeBlue.bind(this)} />
-				</div>
-			);
-		}
-	}
-	class B extends Component {
-		render() {
-			return <p>{this.props.color}</p>;
-		}
-	}
-	const GB = withGlobals(B, 'color');
+    class A extends Component {
+        static contextTypes = {
+            globals: PropTypes.object.isRequired
+        }
+        componentDidMount() {
+            this.context.globals.set('color', 'red');
+        }
+        makeBlue() {
+            this.context.globals.set('color', 'blue');
+        }
+        render() {
+            return (
+                <div>
+                    <div>{this.props.children}</div>
+                    <button onClick={this.makeBlue.bind(this)} />
+                </div>
+            );
+        }
+    }
+    class B extends Component {
+        render() {
+            return <p>{this.props.color}</p>;
+        }
+    }
+    const GB = withGlobals(B, 'color');
 
-	class App extends Component {
-		render() {
-			return (
-				<GlobalsApp>
-					<A>
-						<GB />
-					</A>
-				</GlobalsApp>
-			);
-		}
-	}
+    class App extends Component {
+        render() {
+            return (
+                <GlobalsApp>
+                    <A>
+                        <GB />
+                    </A>
+                </GlobalsApp>
+            );
+        }
+    }
 
-	const wrapper = mount(<App />);
+    const wrapper = mount(<App />);
 
     it('sets the color', () => {
-		expect(wrapper.find('p').text()).toBe('red');
-		wrapper.find('button').simulate('click');
-		expect(wrapper.find('p').text()).toBe('blue');
+        expect(wrapper.find('p').text()).toBe('red');
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('p').text()).toBe('blue');
     });
 
 });
