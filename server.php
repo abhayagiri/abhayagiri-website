c5c593398c0ce13e4e1cc9439e6b2a75dd5d6b0e
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2018-01-07 17:02:27 -0800

    Fix local PHP server for /talks

--- server.php
+++ server.php
@@ -11,24 +11,29 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// Redirect /mahapanel and /th to trailing slash versions.
-if ($uri === '/mahapanel' || $uri === '/th') {
+// Redirect /th to trailing slash versions.
+if ($uri === '/th') {
     header('Location: http://' . $_SERVER['HTTP_HOST'] . $uri . '/');
     return;
 }
 
 // Do not match anything under /new/.
 if (substr($uri, 0, 5) === '/new/' || $uri === '/new') {
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . $uri . '/');
     return false;
+}
+
+// Serve React front-end from new/index.html.
+if (preg_match('_^/(th/)?(gallery|talks)(/.*)?$_', $uri)) {
+    readfile(__DIR__ . '/public/new/index.html');
+    return;
 }
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    // Do not match /mahapanel/ and /th/.
-    if ($uri !== '/mahapanel/' && $uri !== '/th/') {
+    // Do not match /th/.
+    if ($uri !== '/th/') {
         return false;
     }
 }
