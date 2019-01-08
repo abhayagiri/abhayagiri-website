601a7bb8b443b7909d9058b7c7a58779e2732f4c
Adam <adam@kirschbaumdevelopment.com>
2019-01-08 12:26:45 -0500

    add better checks for file and extension to saved observer

--- app/Models/Observers/Talk/SetId3DataObserver.php
+++ app/Models/Observers/Talk/SetId3DataObserver.php
@@ -4,17 +4,18 @@ namespace App\Models\Observers\Talk;
 
 use App\Models\Talk;
 use App\Facades\Id3WriterHelper;
+use Illuminate\Support\Facades\File;
 
 class SetId3DataObserver
 {
     public function saved(Talk $talk)
     {
-        if (str_contains($talk->media_path, 'audio/youtube')) {
-            $fullFileName = base_path('public/media/' . $talk->media_path);
+        $fullFileName = base_path('public/media/' . $talk->media_path);
+        if (File::exists($fullFileName) && File::extension($fullFileName) == 'mp3') {
             Id3WriterHelper::configureWriter($fullFileName, 'id3v2.3', true, false, 'UTF-8');
             Id3WriterHelper::setTag('title', $talk->title_en);
-            Id3WriterHelper::setTag('artist', $talk->author->title_en);
-            Id3WriterHelper::setTag('year', $talk->recorded_on->year);
+            Id3WriterHelper::setTag('artist', optional($talk->author)->title_en);
+            Id3WriterHelper::setTag('year', optional($talk->recorded_on)->year);
             Id3WriterHelper::writeTags();
         }
     }
