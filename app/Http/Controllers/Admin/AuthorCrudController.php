f65921f013b2b58fccdc937561566c837351a51a
Adam <parker14131@gmail.com>
2019-03-27 08:28:11 -0400

    add visiting and rank columns to author

--- app/Http/Controllers/Admin/AuthorCrudController.php
+++ app/Http/Controllers/Admin/AuthorCrudController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\AuthorCrudRequest as StoreRequest;
 use App\Http\Requests\AuthorCrudRequest as UpdateRequest;
 
-class AuthorCrudController extends AdminCrudController {
-
+class AuthorCrudController extends AdminCrudController
+{
     public function setup()
     {
         $this->crud->setModel('App\Models\Author');
         $this->crud->setRoute('admin/authors');
-        $this->crud->orderBy('title_en');
+        $this->crud->orderBy('rank')->orderBy('title_en');
         $this->crud->setEntityNameStrings('author', 'authors');
         $this->crud->allowAccess('revisions');
         $this->crud->with('revisionHistory');
@@ -21,12 +21,21 @@ class AuthorCrudController extends AdminCrudController {
         $this->addImageCrudColumn();
         $this->addTitleEnCrudColumn();
         $this->addTitleThCrudColumn();
+        $this->addBooleanCrudColumn('visiting', 'Visiting?');
         $this->addCheckTranslationCrudColumn();
 
         $this->addTitleEnCrudField();
         $this->addTitleThCrudField();
+        $this->crud->addField([
+            'name' => 'visiting',
+            'label' => 'Visiting',
+            'type' => 'checkbox',
+            'default' => '0',
+            'hint' => 'Check this box if this author is a visiting Ajahn.',
+        ]);
         $this->addCheckTranslationCrudField();
         $this->addImageCrudField();
+        $this->addRankCrudField();
     }
 
     public function store(StoreRequest $request)
