5da4d1f7395b866bfeddcfe7e759ed1e4f7f2b5f
Adam <parker14131@gmail.com>
2019-06-17 07:59:10 -0400

    add conditional for each teacher section

--- new/components/shared/categories/category-pages/teachers.js
+++ new/components/shared/categories/category-pages/teachers.js
@@ -68,15 +68,19 @@ export class CategoryTeachers extends Component {
 
         return !this.state.isLoading ? (
             <div className="mb-5">
-                <article>
-                    <h4>{this.props.t('residents and former residents')}</h4>
-                    <CategoryList list={residents} paginate={5} />
-                </article>
+                { residents.length > 0 &&
+                    <article>
+                        <h4>{this.props.t('residents and former residents')}</h4>
+                        <CategoryList list={residents} paginate={5} />
+                    </article>
+                }
                 <div className="clearfix"></div>
-                <article>
-                    <h4>{this.props.t('visiting ajahns')}</h4>
-                    <CategoryList list={visitors} paginate={5} />
-                </article>
+                { visitors.length > 0 &&
+                    <article>
+                        <h4>{this.props.t('visiting ajahns')}</h4>
+                        <CategoryList list={visitors} paginate={5} />
+                    </article>
+                }
             </div>
          ) : <Spinner />;
     }
