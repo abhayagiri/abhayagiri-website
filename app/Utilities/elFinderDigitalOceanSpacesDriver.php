c936baf6226e409a104bddeec5b2e13a1de764a8
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2019-08-09 14:14:25 +0000

    Force setVisibility to public for Digital Ocean Spaces #114

--- app/Utilities/elFinderDigitalOceanSpacesDriver.php
+++ app/Utilities/elFinderDigitalOceanSpacesDriver.php
@@ -8,6 +8,48 @@ use Barryvdh\elFinderFlysystemDriver\Driver as elFinderFlysystemDriver;
 class elFinderDigitalOceanSpacesDriver extends elFinderFlysystemDriver
 {
     /**
+     * Write a string to a file
+     *
+     * This contains a temporary fix to set the file visibility to public for
+     * Digital Ocean Spaces (#114).
+     *
+     * @param  string $path file path
+     * @param  string $content new file content
+     * @return bool
+     **/
+    protected function _filePutContents($path, $content)
+    {
+        $result = parent::_filePutContents($path, $content);
+        if ($result) {
+            $this->fs->setVisibility($path, 'public');
+        }
+        return $result;
+    }
+
+    /**
+     * Create new file and write into it from file pointer.
+     * Return new file path or false on error.
+     *
+     * This contains a temporary fix to set the file visibility to public for
+     * Digital Ocean Spaces (#114).
+     *
+     * @param  resource $fp file pointer
+     * @param  string $dir target dir path
+     * @param  string $name file name
+     * @param  array $stat file stat (required by some virtual fs)
+     * @return bool|string
+     **/
+    protected function _save($fp, $dir, $name, $stat)
+    {
+        $result = parent::_save($fp, $dir, $name, $stat);
+        if ($result !== false) {
+            $path = $this->_joinPath($dir, $name);
+            $this->fs->setVisibility($path, 'public');
+        }
+        return $result;
+    }
+
+    /**
      * Return required dir's files info.
      *
      * This is an optimization for Digital Ocean Spaces to avoid calling stat
