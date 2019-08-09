47e52ca37e7ba4ad1358b436ee41020017ecde7f
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2019-08-09 17:08:55 +0000

    Add Laravel-style search box

---
+++ resources/views/page/search.blade.php
@@ -0,0 +1,17 @@
+@push('scripts')
+
+<script>
+    var cx = '000972291994809008767:xp7cjel9soe';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+</script>
+
+@endpush
+
+<div class="search" id="search" style="display: none;">
+    <div class="gcse-search"></div>
+</div>
