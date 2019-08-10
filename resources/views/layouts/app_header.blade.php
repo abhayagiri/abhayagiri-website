e1f6958038589b402cdceb67e7fdcd9b4e2ce6af
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2019-08-10 00:01:03 +0000

    Add Laravel-style breadcrumbs

--- resources/views/layouts/app_header.blade.php
+++ resources/views/layouts/app_header.blade.php
@@ -1,5 +1,3 @@
-<!-- TODO Copied (more or less) from react code -->
-
 @include('page/language')
 
 <div id="header" class="clearfix" role="banner">
@@ -8,15 +6,6 @@
 @include('page/nav-menu')
 @include('page/search')
 @include('page/banner')
-
-<div id="breadcrumb-container">
-    <div class="container">
-        <ol class="breadcrumb">
-            <!-- TODO breadcrumbs -->
-            <li class="breadcrumb-item "><a href="{{ lp ('/') }}">{{ __('common.home') }}</a></li>
-            <li class="breadcrumb-item breadcrumb-navpage">{{ $subpage->title }}</li>
-        </ol>
-    </div>
-</div>
+@include('page/breadcrumbs')
 
 </div>
--- resources/views/subpage/show.blade.php
+++ resources/views/subpage/show.blade.php
@@ -2,6 +2,17 @@
 
 @section('title', $subpage->title)
 
+@push('breadcrumbs')
+<li class="breadcrumb-item">
+    <a href="{{ lp($pageMenu['about']->path) }}">
+        {{ $pageMenu['about']->title }}
+    </a>
+</li>
+<li class="breadcrumb-item breadcrumb-navpage">
+    {{ $subpage->title }}
+</li>
+@endpush
+
 @section('main')
 
 <article>
