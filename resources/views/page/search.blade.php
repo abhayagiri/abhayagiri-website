d6082a59be7602177c8897a5283517a0f733ab61
Adam <parker14131@gmail.com>
2019-08-21 15:19:16 -0400

    add search vue component and update search blade file

--- resources/views/page/search.blade.php
+++ resources/views/page/search.blade.php
@@ -1,17 +1,3 @@
-@push('scripts')
-
-<script>
-    var cx = '000972291994809008767:xp7cjel9soe';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-</script>
-
-@endpush
-
 <div class="search" id="search" style="display: none;">
-    <div class="gcse-search"></div>
+    <search-form></search-form>
 </div>
