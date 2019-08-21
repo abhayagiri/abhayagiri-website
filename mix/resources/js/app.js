dfcdeed2bdd37d3dbd79133c2c8ee4ee480e1bfd
Adam <parker14131@gmail.com>
2019-08-21 15:18:26 -0400

    add click outside vue component

--- mix/resources/js/app.js
+++ mix/resources/js/app.js
@@ -19,6 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
+Vue.component('click-outside', require('./components/ClickOutside.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
