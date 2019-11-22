7cdc13f029c34346b127654409555e00943aa9d8
Adam <parker14131@gmail.com>
2019-11-22 07:48:10 -0500

    Update visual style of search and include search indicator and result count

--- mix/resources/js/components/search/Form.vue
+++ mix/resources/js/components/search/Form.vue
@@ -6,6 +6,10 @@
       </div>
       <div class="position-absolute w-100 mx-n3 shadow overflow-auto search-results">
         <div class="bg-light border" v-if="showResults">
+          <p class="pl-2 mt-1 mb-0 w-100 font-italic text-muted" style="font-size: 13px;">{{ resultCountMessage }}</p>
+          <div class="col-12 pt-2" v-if="searching">
+            <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+          </div>
           <div class="col-12" v-for="(result, index) in results" :key="result.id">
             <div class="row" :class="{ 'border-top': index > 0 }">
               <component class="p-2" :is="'search-type-' + result.model_basename" :result="result"></component>
@@ -13,7 +17,7 @@
           </div>
           <div class="col-12">
             <div class="row">
-              <p class="m-0 p-2 w-100 font-italic text-muted text-right">{{ algoliaMessage }}</p>
+              <p class="m-0 p-2 w-100 font-italic text-muted text-right" style="font-size: 12px;">{{ algoliaMessage }}</p>
             </div>
           </div>
         </div>
@@ -32,6 +36,7 @@ export default {
       results: [],
       queryEvent: null,
       showResults: false,
+      searching: false,
     };
   },
 
@@ -49,13 +54,13 @@ export default {
         .get(`/api/search?q=${this.q}&language=${window.Locale}`)
         .then(response => {
           this.results = response.data;
-          if (this.results.length > 0) {
-            this.showResults = true;
-          }
         })
         .catch(error => {
           this.showResults = false;
           console.error('There was an error retrieving the results: ' + error.response.statusText);
+        })
+        .finally(() => {
+          this.searching = false;
         });
     },
 
@@ -76,6 +81,10 @@ export default {
         clearTimeout(this.queryEvent);
       }
 
+      this.results = [];
+      this.showResults = true;
+      this.searching = true;
+
       this.queryEvent = setTimeout(() => {
         this.performSearch();
       }, 750);
@@ -85,6 +94,14 @@ export default {
   computed: {
     algoliaMessage() {
       return (window.Locale == 'en' ? 'Powered by ' : 'K̄hạbkhelụ̄̀xn doy ') + 'Algolia';
+    },
+
+    resultCountMessage() {
+      if(this.searching) {
+        return '';
+      }
+
+      return this.results.length > 0 ? 'Results: ' + this.results.length : 'No results!';
     },
   },
 }
