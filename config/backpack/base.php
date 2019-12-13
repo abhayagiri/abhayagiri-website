5d351557d2bada23cfd233691fb1c7312eeafa38
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2019-12-13 14:47:29 +0000

    Enable Backpack message in footer

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -105,7 +105,7 @@ return [
     'developer_link' => false,
 
     // Show powered by Laravel Backpack in the footer? true/false
-    'show_powered_by' => false,
+    'show_powered_by' => true,
 
     // -------
     // SCRIPTS
