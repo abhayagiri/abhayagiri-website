abhayagiri/abhayagiri-website

commit 7c6157f95c4baf28258987d849e2037bcadc20ca

Add common app partials

app/Models/Traits/PostedAtTrait.php
@@ -2,6 +2,9 @@
 
 namespace App\Models\Traits;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * This trait is for models with the posted_at and draft attributes.
  *
@@ -12,10 +15,10 @@ trait PostedAtTrait
     /**
      * Return a scope culled by not-draft and posted_at not in future.
      *
-     * @param Illuminate\Database\Eloquent\Builder $query
-     * @return Illuminate\Database\Eloquent\Builder
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopePublic($query)
+    public function scopePublic(Builder $query): Builder
     {
         return $query
             ->where($this->getTable() . '.draft', '=', false)
@@ -25,10 +28,10 @@ trait PostedAtTrait
     /**
      * Return a scope orderded by posted_at.
      *
-     * @param Illuminate\Database\Eloquent\Builder $query
-     * @return Illuminate\Database\Eloquent\Builder
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopePostOrdered($query)
+    public function scopePostOrdered(Builder $query): Builder
     {
         return $query
             ->orderBy($this->getTable() . '.posted_at', 'desc');
@@ -37,9 +40,9 @@ trait PostedAtTrait
     /**
      * Returns the local time from posted_at.
      *
-     * @return string or null
+     * @return null|string
      */
-    public function getLocalPostedAtAttribute()
+    public function getLocalPostedAtAttribute(): ?string
     {
         return $this->getLocalDateTimeFrom('posted_at');
     }
@@ -50,7 +53,7 @@ trait PostedAtTrait
      *
      * @return bool
      */
-    public function isPublic()
+    public function isPublic(): bool
     {
         return !$this->draft && $this->posted_at &&
             ($this->posted_at < now());
@@ -59,11 +62,30 @@ trait PostedAtTrait
     /**
      * Sets posted_at from local time.
      *
-     * @param Carbon|string $value
+     * @param  Carbon|string  $value
      * @return string
      */
-    public function setLocalPostedAtAttribute($value)
+    public function setLocalPostedAtAttribute($value): string
     {
         return $this->setLocalDateTimeTo('posted_at', $value);
+    }
+
+    /**
+     * Return whether or not this was modified (at least 2 days) after the
+     * original posted_at date.
+     *
+     * @return bool
+     */
+    public function wasUpdatedAfterPosting(): bool
+    {
+        if ($this->updated_at && $this->posted_at &&
+            ($this->posted_at->diffInDays($this->updated_at) > 2)) {
+            // TODO this is a hardcoded date due to an import that occured on
+            // this date.
+            $minDate = new Carbon('2017-09-15T06:19:58.000000Z');
+            return $this->posted_at->greaterThan($minDate);
+        } else {
+            return false;
+        }
     }
 }

tests/Unit/Models/Traits/PostedAtTraitTest.php
@@ -1,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Models\Traits;
+
+use App\Models\Traits\PostedAtTrait;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Tests\TestCase;
+
+class PostedAtTraitTest extends TestCase
+{
+    public function testWasUpdatedAfterPosting()
+    {
+        $m = new class() extends Model {
+            use PostedAtTrait;
+        };
+        $this->assertFalse($m->wasUpdatedAfterPosting());
+        $m->posted_at = new Carbon('2020-01-01T12:00:00Z');
+        $this->assertFalse($m->wasUpdatedAfterPosting());
+        $m->updated_at = new Carbon('2020-01-01T12:00:00Z');
+        $this->assertFalse($m->wasUpdatedAfterPosting());
+        $m->updated_at = new Carbon('2020-03-01T12:00:00Z');
+        $this->assertTrue($m->wasUpdatedAfterPosting());
+        $m->posted_at = new Carbon('2017-03-01T12:00:00Z');
+        $this->assertFalse($m->wasUpdatedAfterPosting());
+    }
+}

resources/lang/en/common.php
@@ -10,9 +10,13 @@ return [
     */
 
     'abhayagiri_monastery' => 'Abhayagiri Monastery',
+    'back_to' => 'Back to :section',
     'english' => 'English',
     'home' => 'Home',
+    'last_updated' => 'Last Updated',
     'menu' => 'Menu',
+    'page_n' => 'Page :page',
+    'posted' => 'Posted',
     'return_home' => 'Return Home',
     'thai' => 'Thai',
     'search' => 'Search',

resources/lang/th/common.php
@@ -10,9 +10,13 @@ return [
     */
 
     'abhayagiri_monastery' => 'วัดป่าอภัยคีรี',
+    'back_to' => 'กลับไปที่ข่าว:section',
     'english' => 'ภาษาอังกฤษ',
     'home' => 'หน้าหลัก',
+    'last_updated' => 'อัปเดตครั้งล่าสุด',
     'menu' => 'เมนู',
+    'page_n' => 'หน้า :page',
+    'posted' => 'โพสต์เมื่อ',
     'return_home' => 'กลับสู่หน้าแรก',
     'search' => 'ค้นหา',
     'thai' => 'ภาษาไทย',

resources/views/app/article.blade.php
@@ -1,0 +1,16 @@
+<article>
+    <header>
+        <h1>{{ $article->title }}</h1>
+    </header>
+    <section class="body">
+        {!! $article->body_html !!}
+    </section>
+    <aside class="date">
+        <a href="{{ lp($article->getPath()) }}">🔗</a>
+        {{ __('common.posted') }}: @date($article->posted_at)
+        @if ($article->wasUpdatedAfterPosting())
+            <br>
+            {{ __('common.last_updated') }}: @date($article->updated_at)
+        @endif
+    </aside>
+</article>

resources/views/app/back-to.blade.php
@@ -1,0 +1,5 @@
+<p>
+    <a href="{{ lp($pages->current()->path) }}">
+        {{ __('common.back_to', ['section' => $pages->current()->title])}}
+    </a>
+</p>

resources/views/app/index-breadcrumb-title.blade.php
@@ -1,0 +1,13 @@
+@php
+    if ($items->currentPage() > 1) {
+        $pageIndex = __('common.page_n', ['page' => $items->currentPage()]);
+        $title = $pageIndex;
+        $breadcrumbs->addBreadcrumb($pageIndex);
+    } else {
+        $title = null;
+    }
+@endphp
+
+@if ($title)
+    @section('title',  $title)
+@endif

resources/views/app/pagination.blade.php
@@ -1,0 +1,3 @@
+<nav class="pagination">
+    {{ $items->links() }}
+</nav>

commit 921116588331abe3ab1dfefdee083c1e6639f719

Fix local_posted_at accessor
This fixes a problem introduced in commit 7c6157f9.

app/Models/Traits/LocalDateTimeTrait.php
@@ -27,6 +27,7 @@ trait LocalDateTimeTrait
             $value = null;
         }
         $this->attributes[$name] = $value;
+        return $value;
     }
 
     protected function getLocalTimeZone()

app/Models/Traits/PostedAtTrait.php
@@ -65,7 +65,7 @@ trait PostedAtTrait
      * @param  Carbon|string  $value
      * @return string
      */
-    public function setLocalPostedAtAttribute($value): string
+    public function setLocalPostedAtAttribute($value): ?string
     {
         return $this->setLocalDateTimeTo('posted_at', $value);
     }

tests/Unit/Models/Traits/PostedAtTraitTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models\Traits;
 
+use App\Models\Traits\LocalDateTimeTrait;
 use App\Models\Traits\PostedAtTrait;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,22 @@ use Tests\TestCase;
 
 class PostedAtTraitTest extends TestCase
 {
+    public function testAccessors()
+    {
+        $m = new class() extends Model {
+            use LocalDateTimeTrait;
+            use PostedAtTrait;
+        };
+        $this->assertNull($m->posted_at);
+        $this->assertNull($m->local_posted_at);
+        $m->local_posted_at = '2001-01-01 12:00:00';
+        $this->assertEquals('2001-01-01 12:00:00', $m->local_posted_at);
+        $this->assertEquals('2001-01-01 20:00:00', $m->posted_at);
+        $m->local_posted_at = null;
+        $this->assertNull($m->posted_at);
+        $this->assertNull($m->local_posted_at);
+    }
+
     public function testWasUpdatedAfterPosting()
     {
         $m = new class() extends Model {