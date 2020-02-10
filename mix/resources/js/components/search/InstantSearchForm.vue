e492ae7790ece85539559d173487047c8d7eac2c
Adam <parker14131@gmail.com>
2020-02-10 10:57:00 -0500

    Update search related language todos in Vue component to use translations

--- mix/resources/js/components/search/InstantSearchForm.vue
+++ mix/resources/js/components/search/InstantSearchForm.vue
@@ -50,12 +50,12 @@
                                 </template>
                             </ais-hits>
                             <ais-stats>
-                                {{ nbHits }} items found. <!-- TODO localize -->
+                                {{ nbHits }} {{ $t('common.search_result_count') }}.
                             </ais-stats>
                         </template>
                         <template v-else-if="query">
                             <ais-stats>
-                                No items found. <!-- TODO localize -->
+                                {{ $t('common.search_no_results') }}.
                             </ais-stats>
                         </template>
                         <template v-else>
@@ -117,8 +117,7 @@
                 return '';
             },
             searchPlaceholder() {
-                // TODO localize
-                return "Search...";
+                return this.$t('common.search') + '...';
             },
             snippetAttributes() {
                 return [
