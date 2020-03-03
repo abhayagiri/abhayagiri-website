73df6b2556c14d703f2540d56c1caa1fd05a0ffd
Adam <parker14131@gmail.com>
2020-03-03 13:38:04 -0500

    update php translation files for contact

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -1,11 +1,22 @@
 <?php
 
 return [
-
     'from-name' => 'Website Contact Form',
     'success-message' => 'Your message was successfully sent. We will return your message shortly.',
     'confirmation-title' => 'Confirmation Message',
     'confirmation-user-message-title' => 'Your message',
     'confirmation-user-subject' => 'Message from Abhayagiri Buddhist Monastery',
-
+    'contact' => 'Contact',
+    'contact-form' => 'Contact Form',
+    'message-sent' => 'Message sent',
+    'whoops' => 'Whoops',
+    'name' => 'Name',
+    'email-address' => 'Email address',
+    'message' => 'Message',
+    'send-message' => 'Send message',
+    'send-message-loading' => 'Sending your message...',
+    'cancel' => 'Cancel',
+    'back' => 'Back',
+    'confirmation-title' => 'Email Confirmation',
+    'your-message-title' => 'Your message',
 ];
--- resources/lang/th/contact.php
+++ resources/lang/th/contact.php
@@ -1,11 +1,22 @@
 <?php
 
 return [
-
     'from-name' => 'แบบฟอร์มติดต่อทางเว็บไซต์',
     'success-message' => 'ส่งข้อความของคุณประสบความสำเร็จ. เราจะส่งคืนข้อความของคุณในไม่ช้า',
     'confirmation-title' => 'ข้อความยืนยัน',
     'confirmation-user-message-title' => 'ข้อความของคุณ',
     'confirmation-user-subject' => 'ข้อความจากอาราม Abhayagiri Buddhist Monastery',
-
+    'contact' => 'ติดต่อ',
+    'contact-form' => 'แบบฟอร์มติดต่อ',
+    'message-sent' => 'ส่งข้อความ',
+    'whoops' => 'อ๊ะ',
+    'name' => 'ชื่อ',
+    'email-address' => 'ที่อยู่อีเมล',
+    'message' => 'ข่าวสาร',
+    'send-message' => 'ส่งข้อความ',
+    'send-message-loading' => 'กำลังส่งข้อความของคุณ ...',
+    'cancel' => 'ยกเลิก',
+    'back' => 'กลับ',
+    'confirmation-title' => 'ยืนยันอีเมล',
+    'your-message-title' => 'ข้อความของคุณ',
 ];
