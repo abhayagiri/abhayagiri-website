75780a6444d472ce1470ec58591f9e88b14d4ad8
Adam <parker14131@gmail.com>
2020-04-27 10:37:34 -0400

    Update contact blade files to replace vue component functionality

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -3,9 +3,19 @@
 @section('title', __('contact.contact'))
 
 @section('main')
-    <contact-options
-        :preamble="{{ json_encode($preamble) }}"
-        :contact-options="{{ json_encode($contactOptions) }}"
-        :initial-contact-option="{{ json_encode($contactOption) }}"
-    ></contact-options>
+    <div class="contact container">
+        <legend>{{ __('contact.contact') }}</legend>
+
+        <div class="contact-text">{{ optional($preamble)->value }}</div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="contact-options list-group">
+                    @foreach($contactOptions as $contactOption)
+                        <a href="{{ lp('contact/' . $contactOption->slug) }}" class="list-group-item">{{ tp($contactOption, 'name') }}</a>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 @endsection
