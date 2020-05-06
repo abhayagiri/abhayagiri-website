aec2af8366e44ef10f4200d9792a28e01bd9ac03
Adam <parker14131@gmail.com>
2020-05-06 08:47:32 -0400

    Add basic test for RenderMarkdown controller (#135)

---
+++ tests/Feature/Http/Controllers/Admin/Api/RenderMarkdownControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Admin\Api;
+
+use App\Markdown;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RenderMarkdownControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testValidYoutubeInvocation()
+    {
+        $text = 'Test';
+
+        $response = $this
+            ->actingAsAdmin()
+            ->postJson(route('admin.api.render-markdown'), [
+                'text' => $text,
+            ]);
+
+        $response
+            ->assertOk()
+            ->assertJsonPath('html', Markdown::toHtml($text));
+    }
+}
