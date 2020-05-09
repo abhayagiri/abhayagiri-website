abhayagiri/abhayagiri-website

commit 112620509aaf7f879246e51ba496b13dbe00b840

Fix news feed ordering (#164)

app/Models/News.php
@@ -21,9 +21,7 @@ class News extends Model
     use Traits\ImagePathTrait;
     use Traits\MarkdownHtmlTrait;
     use Traits\MediaPathTrait;
-    use Traits\PostedAtTrait {
-        scopePostOrdered as scopePostOrderedWithoutRank;
-    }
+    use Traits\PostedAtTrait;
     use Traits\RevisionableTrait;
 
     /**
@@ -85,18 +83,16 @@ class News extends Model
      */
 
     /**
-     * Return a scope orderded by rank and posted_at.
+     * Return a scope orderded by rank.
      *
      * @param Illuminate\Database\Eloquent\Builder $query
      *
      * @return Illuminate\Database\Eloquent\Builder
      */
-    public function scopePostOrdered($query)
+    public function scopeRankOrdered($query)
     {
         $coalesceSql = DB::raw('COALESCE(' . $this->getTable() . '.rank, 100000) asc');
-        return $query
-            ->orderByRaw($coalesceSql)
-            ->orderBy($this->getTable() . '.posted_at', 'desc');
+        return $query->orderByRaw($coalesceSql);
     }
 
     /**

app/Http/Controllers/NewsController.php
@@ -31,7 +31,7 @@ class NewsController extends Controller
     public function index(): View
     {
         $this->authorize('viewAny', News::class);
-        $news = News::public()->postOrdered()->paginate(10);
+        $news = News::public()->rankOrdered()->postOrdered()->paginate(10);
         return view('news.index', ['news' => $news]);
     }
 

tests/Feature/Http/Controllers/FeedControllerTest.php
@@ -9,6 +9,7 @@ use App\Models\PlaylistGroup;
 use App\Models\Reflection;
 use App\Models\Tale;
 use App\Models\Talk;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Config;
 use SimplePie;
@@ -87,6 +88,27 @@ class FeedControllerTest extends TestCase
         $this->assertFeedIsRss($feed);
         $this->assertFeedIsOkay($feed);
         $this->assertEquals(5, $feed->get_item_quantity());
+    }
+
+    public function testNewsRssFeedOrdering()
+    {
+        $news = News::all();
+        $news[0]->update([
+            'title_en' => 'a1', 'rank' => 1, 'posted_at' => Carbon::parse('2020-01-01')
+        ]);
+        $news[1]->update([
+            'title_en' => 'a2', 'rank' => 2, 'posted_at' => Carbon::parse('2020-02-01')
+        ]);
+        $news[2]->update([
+            'title_en' => 'a3', 'rank' => 3, 'posted_at' => Carbon::parse('2020-03-01')
+        ]);
+        $news[3]->delete();
+        $news[4]->delete();
+        $feed = $this->getFeed(route('news.rss'));
+        $this->assertEquals(3, $feed->get_item_quantity());
+        $this->assertEquals('a3', $feed->get_item(0)->get_title());
+        $this->assertEquals('a2', $feed->get_item(1)->get_title());
+        $this->assertEquals('a1', $feed->get_item(2)->get_title());
     }
 
     public function testReflectionsAtomFeed()
@@ -259,6 +281,7 @@ class FeedControllerTest extends TestCase
     {
         $response = $this->get($url);
         $feed = new SimplePie();
+        $feed->enable_order_by_date(false);
         $feed->set_raw_data($response->getContent());
         $feed->init();
         $feed->handle_content_type($response->headers->get('Content-Type'));

tests/Unit/Models/NewsTest.php
@@ -10,7 +10,7 @@ class NewsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testScopePostOrdered()
+    public function testScopeRankOrdered()
     {
         $news1 = factory(News::class)->create(['id' => 1, 'rank' => null,
                                                'posted_at' => '2019-01-01']);
@@ -18,15 +18,16 @@ class NewsTest extends TestCase
                                                'posted_at' => '2019-07-01']);
         $news3 = factory(News::class)->create(['id' => 3, 'rank' => null,
                                                'posted_at' => '2019-05-01']);
-        $this->assertEquals([2, 3, 1], News::postOrdered()->pluck('id')->toArray());
+        $this->assertEquals([1, 2, 3], News::rankOrdered()->orderBy('id')->pluck('id')->toArray());
+        $this->assertEquals([2, 3, 1], News::rankOrdered()->postOrdered()->pluck('id')->toArray());
         $news1->rank = 1;
         $news1->save();
-        $this->assertEquals([1, 2, 3], News::postOrdered()->pluck('id')->toArray());
+        $this->assertEquals([1, 2, 3], News::rankOrdered()->postOrdered()->pluck('id')->toArray());
         $news3->rank = 1;
         $news3->save();
-        $this->assertEquals([3, 1, 2], News::postOrdered()->pluck('id')->toArray());
+        $this->assertEquals([3, 1, 2], News::rankOrdered()->postOrdered()->pluck('id')->toArray());
         $news3->rank = 2;
         $news3->save();
-        $this->assertEquals([1, 3, 2], News::postOrdered()->pluck('id')->toArray());
+        $this->assertEquals([1, 3, 2], News::rankOrdered()->postOrdered()->pluck('id')->toArray());
     }
 }

commit c76bf0159ce1ee4080118d675479012907d95136

Fix home page news ordering (#164)
This addresses a bug introduced in commit 112620509a.

app/Models/News.php
@@ -104,7 +104,7 @@ class News extends Model
      */
     public function scopeHome($query)
     {
-        return $this->scopePostOrdered($this->scopePublic($query))
+        return $this->scopePostOrdered($this->scopeRankordered($this->scopePublic($query)))
                     ->limit(config('settings.home.news.count'));
     }
 

tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\News;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
 use Tests\TestCase;
 
 class HomeControllerTest extends TestCase
@@ -24,5 +27,24 @@ class HomeControllerTest extends TestCase
         $response = $this->get(route('th.home.index'));
         $response->assertOk()
                  ->assertSee('ดูปฏิทินแบบเต็ม');
+    }
+
+    public function testNewsOrdering()
+    {
+        News::truncate();
+        Config::set('settings.home.news.count', 1);
+        factory(News::class)->create([
+            'title_en' => 'very important news',
+            'rank' => 1,
+            'posted_at' => Carbon::parse('2020-01-01'),
+        ]);
+        factory(News::class)->create([
+            'title_en' => 'less important news',
+            'rank' => null,
+            'posted_at' => Carbon::parse('2020-02-01'),
+        ]);
+        $response = $this->get(route('home.index'));
+        $response->assertSee('very important news')
+                 ->assertDontSee('less important news');
     }
 }