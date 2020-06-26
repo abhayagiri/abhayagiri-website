2b906e80f9b4449e56b32ce8f50d3582ad3c4c02
ABM Devmonk Purple <devmonk-purple@abhayagiri.org>
2020-06-26 09:49:01 -0700

    Fix gallery embed (#117)

--- resources/views/gallery/embed-album.blade.php
+++ resources/views/gallery/embed-album.blade.php
@@ -1,6 +1,6 @@
 <div class="gallery-embed">
     <a href="{{ $album->path }}" class="lightbox"
-        data-gallery-id="1"
+        data-gallery-id="{{ $album->id }}"
         data-gallery-index="0">
         <img src="{{ $album->thumbnail->large_url }}"
              alt="{{ tp($album->thumbnail, 'caption') }}">
