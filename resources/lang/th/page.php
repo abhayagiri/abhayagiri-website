75bdaedf6799dfc0f2a9af405432ac11bde43f25
Fernando Sánchez <34519027+fsmonter@users.noreply.github.com>
2024-01-03 15:14:08 +0000

    Fix bootstrap pagination and footer link texts

--- resources/lang/th/page.php
+++ resources/lang/th/page.php
@@ -11,6 +11,8 @@ return [
         'audio' => 'เสียงธรรม',
         'rss' => 'RSS',
         'subscribe' => 'สมัครเป็นสมาชิก',
+        'subscribe-youtube' => 'สมัครเป็นสมาชิก - YouTube',
+        'subscribe-itunes' => 'สมัครเป็นสมาชิก - iTunes',
         'newsletter' => 'จดหมายข่าว',
     ],
 
